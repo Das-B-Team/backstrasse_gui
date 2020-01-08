fd021d0e616927eebf88aef7023ef2da1d82a7d8
Grant Sut <grantsut@gmail.com>
2020-01-08 16:35:50 +0100

    stern-Vorschau hinzugefuegt

--- Back_GUI_1/MyForm_preview.cpp
+++ Back_GUI_1/MyForm_preview.cpp
@@ -12,11 +12,15 @@
  System::Void BackGUI1::MyForm_preview::MyForm_preview_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
  {
 	 //this->Refresh();
-	 Graphics^ gr = this->CreateGraphics();
 
+	 Graphics^ gr = this->CreateGraphics();
 	 Pen^ pen_black = gcnew Pen(Color::Black);
+
 	 //gr->DrawLine(pen_black, 10, 10, 300, 300);
 	 this->MyForm_preview_zeichne();
+	 
+	 // Dispose of the Graphics object
+	 delete gr;
 	
 
 	 
@@ -25,6 +29,7 @@
  System::Void BackGUI1::MyForm_preview::MyForm_preview_zeichne()
  {
 	 Graphics^ pg = this->CreateGraphics();
+
 	 Pen^ pen_black = gcnew Pen(Color::Black);
 	 Pen^ pen_blue_15 = gcnew Pen(Color::Blue, 15);
 	 Pen^ pen_blue_10 = gcnew Pen(Color::Blue, 10);
@@ -44,7 +49,9 @@
 	 Pen^ pen_red_1 = gcnew Pen(Color::Red, 1);
 	 Pen^ pen_brown_45 = gcnew Pen(Color::Brown, 45);
 	 Pen^ pen_beige_45 = gcnew Pen(Color::Beige, 45);
+
 	 System::Drawing::Font^ fd = gcnew System::Drawing::Font(this->Font, FontStyle::Bold);
+
 	 SolidBrush^ sbrush_yellow = gcnew SolidBrush(Color::Yellow);
 	 SolidBrush^ sbrush_white = gcnew SolidBrush(Color::White);
 	 SolidBrush^ sbrush_darkmagenta = gcnew SolidBrush(Color::DarkMagenta);
@@ -72,6 +79,7 @@
 	 pg->DrawString(p->getTeig()->getMehl().ToString(), fd, sbrush_yellow, 20,60); */
 	 //Ab hier gehts los ;-)
 	 // infos aus Plaetzchen holen
+
 	 String^ Form = p->getShape_as_String();
 	 bool brownie = p->getTeig()->hasKakao() ? brownie = true : brownie = false;
 	 //brownie? pg->DrawString("True", fd, sbrush_yellow, 20, 70): pg->DrawString("Fail", fd, sbrush_yellow, 20, 70);
@@ -91,42 +99,222 @@
 	 if (Form == "Stern")
 	 {
 
-
 		 //try to build a star
-		 // first 
-		 Int32 pa_x, pa_y, pe_x, pe_y, pc_x, pc_y;
-		 pa_x = offset_x + (x + 5) * s / 2;
-		 pa_y = offset_y - (y * -s);
-		 pe_x = offset_x + x * -s / 2;
-		 pe_y = offset_y + y * 8 * s;
-		 pc_x = offset_x + (x + 9) * s / 2;
-		 pc_y = offset_y + (y * 8) * s;
-		 /*Point point_a = Point((x+5)*s + offset_x, (y) *-s +offset_y);
-		 Point point_e = Point((x+1) *-s + offset_x, (y + 8) *s + offset_y);
-		 Point point_c = Point((x + 9) *s + offset_x, (y + 8) *s + offset_y); */
+
+		 Int32 pa_x, pa_y, pb_x, pb_y, pc_x, pc_y;
+		 Int32 pd_x, pd_y, pe_x, pe_y, pf_x, pf_y;
+
+		 Int32 pg_x, pg_y, ph_x, ph_y, pi_x, pi_y;
+		 Int32 pj_x, pj_y, pk_x, pk_y, pl_x, pl_y;
+
+		 Int32 pm_x, pm_y, pn_x, pn_y, po_x, po_y;
+		 Int32 pp_x, pp_y, pq_x, pq_y, pr_x, pr_y;
+
+		 Int32 ps_x, ps_y, pt_x, pt_y, pu_x, pu_y;
+		 Int32 pv_x, pv_y, pw_x, pw_y, px_x, px_y;
+
+		 pa_x = 58; pa_y = 66; pb_x = 210; pb_y = 66; pc_x = 134; pc_y = 196;		 
+		 pd_x = 58; pd_y = 154; pe_x = 134; pe_y = 20; pf_x = 210; pf_y = 154;
+
+		 pg_x = 64; pg_y = 72; ph_x = 204; ph_y = 72; pi_x = 134; pi_y = 190;
+		 pj_x = 64; pj_y = 148; pk_x = 134; pk_y = 26; pl_x = 204; pl_y = 148;
+
+		 pm_x = 70; pm_y = 78; pn_x = 198; pn_y = 78; po_x = 134; po_y = 184;
+		 pp_x = 70; pp_y = 142; pq_x = 134; pq_y = 30; pr_x = 198; pr_y = 142;
+
+		 ps_x = 76; ps_y = 84; pt_x = 192; pt_y = 84; pu_x = 134; pu_y = 178;
+		 pv_x = 76; pv_y = 136; pw_x = 134; pw_y = 36; px_x = 192; px_y = 136;
+		 		
 		 Point point_a = Point(pa_x, pa_y);
-		 Point point_e = Point(pe_x, pe_y);
+		 Point point_b = Point(pb_x, pb_y);
 		 Point point_c = Point(pc_x, pc_y);
-		 System::Console::WriteLine("P_a x\t" + (pa_x).ToString() + " Y\t" + (pa_y).ToString());
-		 System::Console::WriteLine("P_e x\t" + (pe_x).ToString() + " Y\t" + (pe_y).ToString());
-		 System::Console::WriteLine("P_c x\t" + (pc_x).ToString() + " Y\t" + (pc_y).ToString());
-		 // second
-		 Point point_f = Point(x * -s + offset_x, (y + 3) * -s + offset_y);
-		 Point point_b = Point((x + 9) * s + offset_x, (y + 3) * -s + offset_y);
-		 Point point_d = Point((x + 5) + offset_x, (y + 11) * s + offset_y);
+		 Point point_d = Point(pd_x, pd_y);
+		 Point point_e = Point(pe_x, pe_y);
+		 Point point_f = Point(pf_x, pf_y);
 
-		 array<Point>^ Dreieck1 = { point_a, point_c, point_e };
-		 array<Point>^ Dreieck2 = { point_f, point_b, point_d };
+		 Point point_g = Point(pg_x, pg_y);
+		 Point point_h = Point(ph_x, ph_y);
+		 Point point_i = Point(pi_x, pi_y);
+		 Point point_j = Point(pj_x, pj_y);
+		 Point point_k = Point(pk_x, pk_y);
+		 Point point_l = Point(pl_x, pl_y);
 
-		 pg->FillPolygon(sbrush_darkmagenta, Dreieck1);
-		 // pg->FillPolygon(sbrush_yellow, Dreieck2);
-		 pg->FillEllipse(sbrush_yellow, offset_x - 5, offset_y - 5.0, 10.0, 10.0);
+		 Point point_m = Point(pm_x, pm_y);
+		 Point point_n = Point(pn_x, pn_y);
+		 Point point_o = Point(po_x, po_y);
+		 Point point_p = Point(pp_x, pp_y);
+		 Point point_q = Point(pq_x, pq_y);
+		 Point point_r = Point(pr_x, pr_y);
+
+		 Point point_s = Point(ps_x, ps_y);
+		 Point point_t = Point(pt_x, pt_y);
+		 Point point_u = Point(pu_x, pu_y);
+		 Point point_v = Point(pv_x, pv_y);
+		 Point point_w = Point(pw_x, pw_y);
+		 Point point_x = Point(px_x, px_y);
+
+		 array<Point>^ Dreieck1 = { point_a, point_b, point_c };
+		 array<Point>^ Dreieck2 = { point_d, point_e, point_f };
+
+		 array<Point>^ Dreieck3 = { point_g, point_h, point_i };
+		 array<Point>^ Dreieck4 = { point_j, point_k, point_l };
+
+		 array<Point>^ Dreieck5 = { point_m, point_n, point_o };
+		 array<Point>^ Dreieck6 = { point_p, point_q, point_r };
+
+		 array<Point>^ Dreieck7 = { point_s, point_t, point_u };
+		 array<Point>^ Dreieck8 = { point_v, point_w, point_x };
+
+		// pg->FillPolygon(sbrush_beige, Dreieck1);
+		// pg->FillPolygon(sbrush_beige, Dreieck2);
+
+		 //System::Console::WriteLine("P_a x\t" + (pa_x).ToString() + " y\t" + (pa_y).ToString());
+		 //System::Console::WriteLine("P_b x\t" + (pb_x).ToString() + " y\t" + (pb_y).ToString());
+		 //System::Console::WriteLine("P_c x\t" + (pc_x).ToString() + " y\t" + (pc_y).ToString());
+
+		 brownie ? pg->FillPolygon(sbrush_brown, Dreieck1)
+		 : pg->FillPolygon(sbrush_beige, Dreieck1) ;
+		 brownie ? pg->FillPolygon(sbrush_brown, Dreieck2)
+		 : pg->FillPolygon(sbrush_beige, Dreieck2) ;
+
+		 if (p->hat_verzierung())
+		 {
+			 Int16 i = 0;
+			 for each (auto v in p->getVerzierung())
+			 {
+				 pg->DrawString(v->getName(), fd, sbrush_darkmagenta, 15, i * 15 + 70);
+				 switch (v->getFarbe())
+				 {
+				 case(Verzierung::Color::Blue):
+					 if (i == 0) // erste verzierung
+					 {
+					 
+					 pg->FillPolygon(sbrush_blue, Dreieck3);
+					 pg->FillPolygon(sbrush_blue, Dreieck4);
+
+					 }
+					 if (i == 1) // zweite verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_blue, Dreieck5);
+					 pg->FillPolygon(sbrush_blue, Dreieck6);
+
+					 }
+					 if (i == 2) // erste verzierung
+					 {
+			
+					 pg->FillPolygon(sbrush_blue, Dreieck7);
+					 pg->FillPolygon(sbrush_blue, Dreieck8);
+
+					 }
+					 break;
+				 case(Verzierung::Color::Green):
+					 if (i == 0) // erste verzierung
+					 {
+					 
+					 pg->FillPolygon(sbrush_green, Dreieck3);
+					 pg->FillPolygon(sbrush_green, Dreieck4);
+
+					 }
+					 if (i == 1) // zweite verzierung
+					 {
+					 
+
+					 pg->FillPolygon(sbrush_green, Dreieck5);
+					 pg->FillPolygon(sbrush_green, Dreieck6);
+
+					 }
+					 if (i == 2) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_green, Dreieck7);
+					 pg->FillPolygon(sbrush_green, Dreieck8);
+
+					 }
+					 break;
+				 case(Verzierung::Color::Pink):
+					 if (i == 0) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_pink, Dreieck3);
+					 pg->FillPolygon(sbrush_pink, Dreieck4);
+
+					 }
+					 if (i == 1) // zweite verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_pink, Dreieck5);
+					 pg->FillPolygon(sbrush_pink, Dreieck6);
+
+					 }
+					 if (i == 2) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_pink, Dreieck7);
+					 pg->FillPolygon(sbrush_pink, Dreieck8);
+
+					 }
+					 break;
+				case(Verzierung::Color::Red):
+					 if (i == 0) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_red, Dreieck3);
+					 pg->FillPolygon(sbrush_red, Dreieck4);
+
+					 }
+					 if (i == 1) // zweite verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_red, Dreieck5);
+					 pg->FillPolygon(sbrush_red, Dreieck6);
+
+					 }
+					 if (i == 2) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_red, Dreieck7);
+					 pg->FillPolygon(sbrush_red, Dreieck8);
+
+					 }
+					 break;
+				case(Verzierung::Color::White):
+					 if (i == 0) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_white, Dreieck3);
+					 pg->FillPolygon(sbrush_white, Dreieck4);
+
+					 }
+					 if (i == 1) // zweite verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_white, Dreieck5);
+					 pg->FillPolygon(sbrush_white, Dreieck6);
+
+					 }
+					 if (i == 2) // erste verzierung
+					 {
+
+					 pg->FillPolygon(sbrush_white, Dreieck7);
+					 pg->FillPolygon(sbrush_white, Dreieck8);
+
+					 }
+					 break;
+				
+
+				 }
+				 i++;
+			 }
+		 }
+
 	 }
 	 if (Form == "Gringel")
 	 {
 		 // Gringel zeichnen
 
-		 brownie ? pg->DrawEllipse(pen_brown_45, offset_x, offset_y, 120.0, 120.0) : pg->DrawEllipse(pen_beige_45, offset_x, offset_y, 120.0, 120.0);
+		 brownie ? pg->DrawEllipse(pen_brown_45, offset_x, offset_y, 120.0, 120.0) 
+			 : pg->DrawEllipse(pen_beige_45, offset_x, offset_y, 120.0, 120.0);
 		 if (p->hat_verzierung())
 		 {
 			 Int16 i = 0;
@@ -216,7 +404,8 @@
 	 if (Form == "Rund")
 	 {
 		 // Rundes Plaetzchen zeichnen
-		 brownie ? pg->FillEllipse(sbrush_brown, offset_x, offset_y, 120.0, 120.0) : pg->FillEllipse(sbrush_beige, offset_x, offset_y, 120.0, 120.0);
+		 brownie ? pg->FillEllipse(sbrush_brown, offset_x, offset_y, 120.0, 120.0) 
+			 : pg->FillEllipse(sbrush_beige, offset_x, offset_y, 120.0, 120.0);
 		 if (p->hat_verzierung())
 		 {
 			 Int16 i = 0;
@@ -308,7 +497,8 @@
 	 {
 		 // Quatratisches Plaetzchen zeichnen
 		
-			 brownie ? pg->FillRectangle(sbrush_brown, offset_x, offset_y, 120.0, 120.0) : pg->FillRectangle(sbrush_beige, offset_x, offset_y, 120.0, 120.0);
+			 brownie ? pg->FillRectangle(sbrush_brown, offset_x, offset_y, 120.0, 120.0) 
+				 : pg->FillRectangle(sbrush_beige, offset_x, offset_y, 120.0, 120.0);
 			 if (p->hat_verzierung())
 			 {
 				 Int16 i = 0;
@@ -321,7 +511,6 @@
 						 if (i == 0) // erste verzierung
 						 {
 							 pg->FillRectangle(sbrush_blue, offset_x + 5, offset_y + 5, 110.0, 110.0);
-
 						 }
 						 if (i == 1) // zweite verzierung
 						 {
@@ -397,4 +586,6 @@
 
 		 }
 	 }
+
+	 
  
--- Back_GUI_1/Plaetzchen.h
+++ Back_GUI_1/Plaetzchen.h
@@ -33,43 +33,52 @@ public:
 			   bake_temperature temperature 
 			   sh_pe shape of the cookie
 			   is_brownie when brown cookies are desired*/
+
 	Plaetzchen(Single size_x, Single size_y, Single bake_time, Single bake_temperature, Form_cookie sh_pe, bool is_brownie);
 
 	/**getVerzierung() return a List of possible topit's on the cookie
 	Return: List<Verzierung>^ */
+
 	List<Verzierung^>^ getVerzierung();
 
 	/**addverzierung(Verzirung^ v) add a 'Verzierung' to a List
 	Parameter: v pointer to Verzierung
 	Return: bool return true when adding was sucessfull otherweise false*/
+
 	bool addVerzierung(Verzierung^);
 	
 	/**getTeig() return a pointer to the Teig object which belongs to this cookie
 	Return: Teig^*/
+
 	Teig^ getTeig();
 
 	/**getGroesseX() gives the space which one cookkie occupie in X direction
 	Parameter: none
 	Return: x in mm*/
+
 	Single getGroesseX();
 
 	/**getGroesseY() gives the space which one cookkie occupie in Y direction
 	Parameter: none
 	Return: y in mm*/
+
 	Single getGroesseY();
 
 	/**hat_verzierung() return true if there are any decoration otherwise false
 	Return: true if decoration is available */
+
 	bool hat_verzierung();
 
 	/** berechneGewicht() calculate the wight of a cookie with the assumption that
 	the cookie took 75% from the space GroesseX and GroesseY
 	40mm x 40mm ~ 33g cookie weight
 	Return: Single (float) weight of one cookie*/
+
 	Single berecheGewicht();
 	/** getShape_as_String
 	 Parameter: none
 	 Return: the shape as a String i.e "Stern"*/
+
 	String ^ getShape_as_String();
 
 	
